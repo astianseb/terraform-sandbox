d610249a1716937bb61a6abd7533cb2973281414
astianseb <callmeseb@gmail.com>
2020-08-10 08:41:44 +0200

    First commit

---
+++ 01-sample-instance/main.tf
@@ -0,0 +1,30 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "vm_instance" {
+    name = "tf-generated-instance-1"
+    machine_type = "f1-micro"
+    
+    scheduling {
+        automatic_restart = false
+        preemptible = true
+        }
+    
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-9"
+        }
+        } 
+
+    network_interface {
+        # A default network is created for all GCP projects
+        network = "default"
+        access_config {
+
+        }
+        }
+}
+
---
+++ 01-sample-instance/terraform.tfvars
@@ -0,0 +1,3 @@
+project = "sg-sandbox-285909"
+region = "europe-west1"
+zone = "europe-west1-b"
