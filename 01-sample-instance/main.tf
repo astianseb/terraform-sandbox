a9fb2df8e86f5604af16994cdf959456eb48ccec
Seb <callmeseb@gmail.com>
2020-10-13 13:52:48 +0000

    Name change

--- 01-sample-instance/main.tf
+++ 01-sample-instance/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-    name = "tf-generated-instance-cb"
+    name = "tf-cloudbuild"
     machine_type = "f1-micro"
     
     scheduling {
