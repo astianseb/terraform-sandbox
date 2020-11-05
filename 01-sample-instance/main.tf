5da4aaf3cea0f38c4e05842b048abdc956aa2ed8
Seb <callmeseb@gmail.com>
2020-11-05 13:41:57 +0000

    Zmiana nazwy na 3

--- 01-sample-instance/main.tf
+++ 01-sample-instance/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-    name = "tf-cloudbuild2"
+    name = "tf-cloudbuild-3"
     machine_type = "f1-micro"
     
     scheduling {
